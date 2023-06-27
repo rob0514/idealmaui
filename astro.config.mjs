92e89a3352318a32d7e0a80af50fb568a4b4aa33
Rob <babblespring@gmail.com>
2023-06-27 12:18:05 -1000

    c4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rob0514.github.io',
+  base: '/idealmaui'
   integrations: [tailwind({
     applyBaseStyles: false,
   })]
